0c16aa44c72204244aa047bf6722553cfee7dd7b
Noe <Noe.BERDOZ@cpnv.ch>
2020-05-11 14:10:36 +0200

    First Vue With Components

--- Components/Search.js
+++ Components/Search.js
@@ -1,19 +1,32 @@
 import React from 'react'
-import { StyleSheet, View, Button, TextInput } from 'react-native'
+import { StyleSheet, View, Button, TextInput, FlatList, Text } from 'react-native'
+import films from '../Helpers/filmsData'
+import FilmItem from './FilmItem';
+
+
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 class Search extends React.Component {
     render() {
         return (
-            <View>
+            <View style={styles.main_container}>
                 <TextInput style={ styles.textinput } placeholder="Titre du film"/>
                 <Button title="Rechercher" onPress={() => {}}/>
+                <FlatList
+                    data={films}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}) => <FilmItem/>}
+                />
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    main_container: {
+        flex: 1,
+        marginTop:20
+    },
     textinput: {
         backgroundColor: Colors.light,
     }
