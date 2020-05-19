6fbbdbdf6fc356245673939826247ae1a62f5845
Noe <Noe.BERDOZ@cpnv.ch>
2020-05-19 09:11:35 +0200

    Infinite load list function & ActivityIndicator implementation & Images

--- Components/Search.js
+++ Components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, Button, TextInput, FlatList, Text } from 'react-native'
+import { StyleSheet, View, Button, TextInput, FlatList, Text, ActivityIndicator } from 'react-native'
 import films from '../Helpers/filmsData'
 import FilmItem from './FilmItem'
 
@@ -10,15 +10,37 @@ class Search extends React.Component {
 
     constructor(props) {
         super(props);
+        this.page = 0
+        this.totalPages = 0
         this.state = {
             films: [],
+            isLoading: false // Dont default show the loading circle
         }
         this.searchedText = ""
     }
 
     _loadFilms() {
+        this.setState({ isLoading: true }) // Lauching loading circle
         if (this.searchedText.length > 0) {
-            getFilmsFromApiWithSearchedText(this.searchedText).then(data => this.setState({ films: data.results }))
+            getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => {
+                this.page = data.page
+                this.totalPages = data.total_pages
+                this.setState({
+                    films: [ ...this.state.films, ...data.results ], // ... = clone
+                    isLoading: false // Ending lauching circle
+                })
+
+            })
+        }
+    }
+
+    _displayLoading() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size='large' />
+                </View>
+            )
         }
     }
 
@@ -26,17 +48,37 @@ class Search extends React.Component {
         this.searchedText = text
     }
 
+    _searchedFilms() {
+        this.page = 0
+        this.totalPages = 0
+        this.setState({
+            films: []
+        }, () => {
+            // J'utilise la paramètre length sur mon tableau de films pour vérifier qu'il y a bien 0 film
+            console.log("Page : " + this.page + " / TotalPages : " + this.totalPages + " / Nombre de films : " + this.state.films.length)
+            this._loadFilms()
+        })
+
+    }
+
     render() {
-        console.log("RENDER")
         return (
             <View style={styles.main_container}>
-                <TextInput onChangeText={(text) => this._searchTextInputChanged(text)} style={styles.textinput} placeholder="Titre du film"/>
-                <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+                <TextInput onSubmitEditing={() => this._searchedFilms()} onChangeText={(text) => this._searchTextInputChanged(text)} style={styles.textinput} placeholder="Titre du film"/>
+                <Button title='Rechercher' onPress={() => this._searchedFilms()}/>
                 <FlatList
                     data={this.state.films}
                     keyExtractor={(item) => item.id.toString()}
+                    onEndReachThreashold={0.5}
+                    onEndReached={() => {
+                        if (this.page < this.totalPages) {
+                            this._loadFilms()
+                        }
+                        console.log("onEndReached")
+                    }}
                     renderItem={({item}) => <FilmItem film={item}/>}
                 />
+                {this._displayLoading()}
             </View>
         )
     }
@@ -54,6 +96,15 @@ const styles = StyleSheet.create({
         borderColor: '#000000',
         borderWidth: 1,
         paddingLeft: 5
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 });
 
